04883ba5c87686390def14e8d497bd910f5df43b
Rustan Corpuz <rustanacexd@gmail.com>
2015-10-23 23:23:26 +0800

    busienssitemdetail recyclerview header initial build

--- app/src/main/java/com/trytara/tara/adapters/business/BusinessItemDetailReviewsAdapter.java
+++ app/src/main/java/com/trytara/tara/adapters/business/BusinessItemDetailReviewsAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -14,60 +15,76 @@ import com.trytara.tara.models.Business;
 
 import java.util.List;
 
-public class BusinessItemDetailReviewsAdapter extends RecyclerView.Adapter<BusinessItemDetailReviewsAdapter.ViewHolder> {
-    private static final String TAG = "BusinessItemDetailReviewsAdapter";
-
-    private List<Business.Review> mDataSet;
-    private static Context mContext;
+public class BusinessItemDetailReviewsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final int TYPE_HEADER = 0;
+    private static final int TYPE_ITEM = 1;
+    String[] data;
 
     public BusinessItemDetailReviewsAdapter() {
-//        mContext = context;
-//        mDataSet = dataSet;
+        this.data = data;
     }
 
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == TYPE_ITEM) {
+            //inflate your layout and pass it to view holder
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.business_item_detail_review_row, parent, false);
+            return new VHItem(v);
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-
-
-        public ViewHolder(View v) {
-            super(v);
-            // Define click listener for the ViewHolder's View.
-            v.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    /*Intent i = new Intent(mContext, BusinessDetailActivity.class);
-                    mContext.startActivity(i);*/
-                }
-            });
-
-
+        } else if (viewType == TYPE_HEADER) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.rv_business_item_menu_reviews_header, parent, false);
+            return new VHHeader(v);
         }
 
-
+        throw new RuntimeException("there is no type that matches the type " + viewType + " + make sure your using types correctly");
     }
 
-
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        // Create a new view.
-        View v = LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.business_item_detail_review_row, viewGroup, false);
-
-        return new ViewHolder(v);
-    }
-
-
-    @Override
-    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
-        //Log.d(TAG, "Element " + position + " set.");
-
-        //Business.Review review = mDataSet.get(position);
-
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof VHItem) {
+            //String dataItem = getItem(position);
+            //cast holder to VHItem and set data
+        } else if (holder instanceof VHHeader) {
+            //cast holder to VHHeader and set data for header.
+        }
     }
 
     @Override
     public int getItemCount() {
-//        return mDataSet.size();
-        return 10;
+        return 5;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (isPositionHeader(position))
+            return TYPE_HEADER;
+
+        return TYPE_ITEM;
+    }
+
+    private boolean isPositionHeader(int position) {
+        return position == 0;
+    }
+
+    private String getItem(int position) {
+        return data[position - 1];
+    }
+
+    public static class VHItem extends RecyclerView.ViewHolder {
+        TextView title;
+
+        public VHItem(View itemView) {
+            super(itemView);
+        }
+    }
+
+    public static class VHHeader extends RecyclerView.ViewHolder {
+        Button button;
+
+        public VHHeader(View itemView) {
+            super(itemView);
+        }
     }
 }
